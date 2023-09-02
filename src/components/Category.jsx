69cef00dd9f5979f3748721b8476a209c0991568
Kiranabrar <kiran.cs577@gmail.com>
2023-09-02 12:24:56 +0500

    update fetch function

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -5,8 +5,7 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 
 const Category = () => {
-
-  const [urlcat] = useState("https:/www.themealdb.com/api/json/v1/1/categories.php");
+ /*{ const [urlcat] = useState("https:/www.themealdb.com/api/json/v1/1/categories.php");
   const [items, setItems] = useState();
   const [shows, setShows] = useState(false);
 
@@ -16,6 +15,22 @@ const Category = () => {
       setShows(true);
     })
   }, [urlcat])
+}*/
+  
+
+
+  const [shows, setShows] = useState(false);
+  const [items, setItems]=useState([]);
+  useEffect(() =>{
+    setShows(true);
+  getItems();
+  },[]);
+  async function getItems(){
+    const response=await fetch('https:/www.themealdb.com/api/json/v1/1/categories.php');
+    const data=await response.json();
+    setItems(data.categories);
+  }
+
 
   return (
     <>
